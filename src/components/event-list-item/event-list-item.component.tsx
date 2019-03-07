3546f559ed8c4a39455c0cd2b9a769636cff1e8d
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2019-03-07 16:31:19 +0200

    Item card component

--- src/components/event-list-item/event-list-item.component.tsx
+++ src/components/event-list-item/event-list-item.component.tsx
@@ -1,42 +1,12 @@
 import React, { Component } from 'react';
 import './event-list-item.component.scss';
-import { Card, Row, Col } from 'antd';
+import ItemCard from '../item-card/item-card.component';
 
 class EventListItem extends Component {
     render() {
         return (
             <div className="event-list-item" style={{ backgroundImage: this.GenerateGradient() }}>
-                <div className="item-card">
-                    <Row>
-                        <Col span={5}>
-                            <div className="item-card-picture" />
-                        </Col>
-                        <Col span={19}>
-                            <div className="item-card-title">
-                                Sergiu cauta colegi pentru alergat
-                                <p>
-                                    Vivamus pellentesque orci et gravida laoreet. Phasellus gravida
-                                    efficitur elit sed rutrum.
-                                </p>
-                            </div>
-                        </Col>
-                    </Row>
-
-                    <Row className="item-card-icons">
-                        <Col span={8}>
-                            <span className="icon mdi mdi-map-marker" />
-                            <span className="text">Constanta</span>
-                        </Col>
-                        <Col span={10} className="text-center">
-                            <span className="icon mdi mdi-alarm" />
-                            <span className="text">10/12/2019, 13:30</span>
-                        </Col>
-                        <Col span={6} className="text-right">
-                            <span className="icon mdi mdi-account-group" />
-                            <span className="text">7/12</span>
-                        </Col>
-                    </Row>
-                </div>
+                <ItemCard />
             </div>
         );
     }
---
+++ src/components/item-card/item-card.component.tsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import './item-card.component.scss';
+import { Row, Col } from "antd";
+
+class ItemCard extends Component{
+    render(){
+        return (
+            <div className="item-card">
+                    <Row>
+                        <Col span={5}>
+                            <div className="item-card-picture" />
+                        </Col>
+                        <Col span={19}>
+                            <div className="item-card-title">
+                                Sergiu cauta colegi pentru alergat
+                                <p>
+                                    Vivamus pellentesque orci et gravida laoreet. Phasellus gravida
+                                    efficitur elit sed rutrum.
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+
+                    <Row className="item-card-icons">
+                        <Col span={8}>
+                            <span className="icon mdi mdi-map-marker" />
+                            <span className="text">Constanta</span>
+                        </Col>
+                        <Col span={10} className="text-center">
+                            <span className="icon mdi mdi-alarm" />
+                            <span className="text">10/12/2019, 13:30</span>
+                        </Col>
+                        <Col span={6} className="text-right">
+                            <span className="icon mdi mdi-account-group" />
+                            <span className="text">7/12</span>
+                        </Col>
+                    </Row>
+                </div>
+        )
+    }
+}
+
+export default ItemCard;
