13e90ce8a704c1f088c26a796516db0a72b473f5
Danut Ilie <daniel_ilie97@yahoo.com>
2019-09-10 11:51:47 +0300

    login with facebook

--- src/contracts/view-models/login.view-model.ts
+++ src/contracts/view-models/login.view-model.ts
@@ -1,6 +1,7 @@
 export class LoginViewModel {
     Email!: string;
     Password!: string;
+    IsLogWithFb!:boolean;
 
     constructor(email: string, password: string) {
         this.Email = email;
