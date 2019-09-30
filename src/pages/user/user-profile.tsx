ee4fd29f8c503d52f2740d81d23e8ff3b8ae575a
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2019-09-30 11:29:49 +0300

    User profile design

--- src/pages/user/user-profile.tsx
+++ src/pages/user/user-profile.tsx
@@ -1,73 +1,114 @@
 import React, { Component } from 'react';
 import './user-profile.scss';
-import { Input } from 'antd';
+import { Input, Card, Row, Col } from 'antd';
 import { UserService } from '../../business/services';
 import { UserViewModel } from '../../contracts/view-models/user-view.model';
 import moment from 'moment';
 
 class UserProfilePage extends Component {
-    state = {
-        userDetails: new UserViewModel(),
-    };
+  state = {
+    userDetails: new UserViewModel(),
+  };
 
-    async componentDidMount() {
-        let userDetails = await UserService.Get();
-        this.setState({
-            userDetails: userDetails,
-        });
-    }
-    render() {
-        return (
-            <div className="user-profile full-height">
-                <div className="page-sections">
-                    <div className="page-section page-section-large">
-                        <div className="text-align">
-                            <div
-                                className="dot avatar"
-                                style={{
-                                    backgroundImage:
-                                        `url(${this.state.userDetails.user.Url})`,
-                                }}
-                            ></div>
-                        </div>
-                        <div>
-                            <div className="name">
-                                {this.state.userDetails.user.FirstName||''} {this.state.userDetails.user.LastName||''}
-                            </div>
-                            <label>Despre</label>
-                            <div className="inline-input-wrapper">
-                                <span>Inaltime:</span>
-                                <Input
-                                    readOnly
-                                    value={this.state.userDetails.details.Height||''}
-                                ></Input>
-                            </div>
-                            <div className="inline-input-wrapper">
-                                <span>Greutate:</span>
-                                <Input
-                                    readOnly
-                                    value={this.state.userDetails.details.Weight||''}
-                                ></Input>
-                            </div>
-                            <div className="inline-input-wrapper">
-                                <span>Zi de naspere:</span>
-                                <Input readOnly value={this.state.userDetails.details.Day||''}></Input>
-                            </div>
-                            <div className="inline-input-wrapper">
-                                <span>Varsta:</span>
-                                <Input readOnly value={moment().diff(moment(this.state.userDetails.details.Day, 'DD/MM/YYYY'), 'years')||''}></Input>
-                            </div>
-                            <label>Contact</label>
-                            <div className="inline-input-wrapper">
-                                <span>Email:</span>
-                                <Input readOnly value={this.state.userDetails.user.Email||''}></Input>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  async componentDidMount(): Promise<void> {
+    let userDetails: UserViewModel = await UserService.Get();
+    this.setState({
+      userDetails: userDetails,
+    });
+  }
+
+  render(): any {
+    const loading: boolean = !this.state.userDetails.user;
+    const age: number | string =
+      moment().diff(moment(this.state.userDetails.details.Day, 'DD/MM/YYYY'), 'years') || '';
+
+    return (
+      <div className="user-profile full-height">
+        <div className="page-sections">
+          <div className="page-section page-section-large">
+            <div className="text-align">
+              <div
+                className="dot avatar"
+                style={{
+                  backgroundImage: `url(${this.state.userDetails.user.Url})`,
+                }}
+              ></div>
             </div>
-        );
-    }
+
+            <div className="name">
+              {this.state.userDetails.user.FirstName || ''}{' '}
+              {this.state.userDetails.user.LastName || ''}
+            </div>
+
+            <div className="user-profile-content">
+              <Card loading={loading}>
+                <div className="margin-bottom">
+                  <h3>Despre</h3>
+                  <div className="inline-input-wrapper">
+                    <Row type="flex" justify="space-around" align="middle">
+                      <Col span={8}>
+                        <label>Inaltime:</label>
+                      </Col>
+                      <Col span={16}>
+                        <Input readOnly value={this.state.userDetails.details.Height || ''}></Input>
+                      </Col>
+                    </Row>
+                  </div>
+
+                  <div className="inline-input-wrapper">
+                    <Row type="flex" justify="space-around" align="middle">
+                      <Col span={8}>
+                        <label>Greutate:</label>
+                      </Col>
+                      <Col span={16}>
+                        <Input readOnly value={this.state.userDetails.details.Weight || ''}></Input>
+                      </Col>
+                    </Row>
+                  </div>
+
+                  <div className="inline-input-wrapper">
+                    <Row type="flex" justify="space-around" align="middle">
+                      <Col span={8}>
+                        <label>Zi de naspere:</label>
+                      </Col>
+                      <Col span={16}>
+                        <Input readOnly value={this.state.userDetails.details.Day || ''}></Input>
+                      </Col>
+                    </Row>
+                  </div>
+
+                  <div className="inline-input-wrapper">
+                    <Row type="flex" justify="space-around" align="middle">
+                      <Col span={8}>
+                        <label>Varsta:</label>
+                      </Col>
+                      <Col span={16}>
+                        <Input readOnly value={age}></Input>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+
+                <div>
+                  <h3>Contact</h3>
+                  <div className="inline-input-wrapper">
+                    <Row type="flex" justify="space-around" align="middle">
+                      <Col span={8}>
+                        <label>Email:</label>
+                      </Col>
+                      <Col span={16}>
+                        <Input readOnly value={this.state.userDetails.user.Email || ''}></Input>
+                      </Col>
+                    </Row>
+                  </div>
+                </div>
+              </Card>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default UserProfilePage;
