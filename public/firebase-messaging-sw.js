448052d4d61cd64f2fe6b2f09f49d3000498dab6
Sergiu Butnarasu <sergiu.butnarasu@gmail.com>
2019-10-16 11:33:11 +0300

    trying firebase setBackgroundMessageHandler

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,31 @@
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
 var firebaseConfig = {
-    apiKey: "AIzaSyDet8nQkb4kEGzdJtvDo4JA9GvXZ-7kUxg",
-    authDomain: "nohai-pushnotification.firebaseapp.com",
-    databaseURL: "https://nohai-pushnotification.firebaseio.com",
-    projectId: "nohai-pushnotification",
-    storageBucket: "",
-    messagingSenderId: "44767533362",
-    appId: "1:44767533362:web:522cde2738eae6fa"
+  apiKey: 'AIzaSyDet8nQkb4kEGzdJtvDo4JA9GvXZ-7kUxg',
+  authDomain: 'nohai-pushnotification.firebaseapp.com',
+  databaseURL: 'https://nohai-pushnotification.firebaseio.com',
+  projectId: 'nohai-pushnotification',
+  storageBucket: '',
+  messagingSenderId: '44767533362',
+  appId: '1:44767533362:web:522cde2738eae6fa',
 };
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  var notificationTitle = payload.data.title; //or payload.notification or whatever your payload is
+  var notificationOptions = {
+    body: payload.data.body,
+    icon: 'https://no-hai.ro/assets/app-icon.png',
+    data: { url: payload.data.click_action },
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(self.clients.openWindow(event.notification.data));
+});
