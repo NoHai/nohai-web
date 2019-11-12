3663d409c99dbb7774fc9869abec26f9874bb553
Danut Ilie <daniel_ilie97@yahoo.com>
2019-11-12 17:15:25 +0200

    add filter modal

--- src/pages/event/search-events/search-events.page.tsx
+++ src/pages/event/search-events/search-events.page.tsx
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 import './search-events.page.scss';
 import EventList from '../../../components/event-list/event-list.component';
 import { FindEventRequest } from '../../../contracts/requests/find-event.request';
-import { EventDetailsViewModel } from '../../../contracts/models';
+import { EventDetailsViewModel, ListModel } from '../../../contracts/models';
 import LoadingHelper from '../../../helpers/loading.helper';
 import { EventService } from '../../../business/services';
-import { Input } from 'antd';
-
+import { Row, Col, Modal, Select, DatePicker } from 'antd';
+import { CommonService } from '../../../business/services/common.service';
+import { SportModel } from '../../../contracts/models/sport.model';
+import moment from 'moment';
+const { Option } = Select;
 class SearchEventsPage extends Component {
   public eventRequest = new FindEventRequest();
   public eventDetilsContainer = new Array<EventDetailsViewModel>();
+  public sports = new ListModel<SportModel>();
 
   state = {
     eventDetails: new Array<EventDetailsViewModel>(),
     hasMoreItems: true,
     pageIndex: 0,
     total: 0,
+    showFilter: false,
   };
 
   constructor(props: any) {
@@ -25,6 +30,7 @@ class SearchEventsPage extends Component {
 
   async componentDidMount() {
     await this.getEvents();
+    this.sports = await CommonService.GetSports();
   }
 
   public render() {
@@ -32,33 +38,89 @@ class SearchEventsPage extends Component {
       <div className="full-height">
         <div className="page-sections">
           <div className="page-section">
-            <div className="icon mdi mdi-magnify">
-              <Input onChange={(e)=>this.onSearchChange(e)} placeholder="Cauta evenimente..."></Input>
-            </div>
-            <div className="icon mdi mdi-filter-menu"></div>
+            <Row className="inline-input-wrapper">
+              <Col span={12}>
+                <input
+                  className="input"
+                  onChange={e => this.onSearchChange(e)}
+                  placeholder="Cauta evenimente..."
+                ></input>
+                <span className="icon mdi mdi-magnify"></span>
+              </Col>
+              <Col span={12} className="text-right">
+                <div
+                  className="icon mdi mdi-filter-menu"
+                  onClick={() => this.toggleShowModal()}
+                ></div>
+              </Col>
+            </Row>
           </div>
-          <div className="page-section-large">
+          <div className="page-section page-section-large">
             <EventList
               eventDetails={this.state.eventDetails}
               hasMoreItems={this.state.hasMoreItems}
               onEventsDetailsChange={() => this.getEvents()}
             />
+            <Modal
+              title="Filtreaza evenimentele"
+              visible={this.state.showFilter}
+              onOk={() => this.applyFilter()}
+              onCancel={() => this.toggleShowModal()}
+            >
+              <label>Dupa sport:</label>
+              <div>
+                <Select
+                  className="select"
+                  size="default"
+                  placeholder="Selecteaza Sportul"
+                  onChange={(value: string) => {
+                    if (this.eventRequest.sports) this.eventRequest.sports.push(value);
+                  }}
+                >
+                  {this.sports.Data.map((element: any, index: any) => (
+                    <Option key={index} value={element.Id}>
+                      {element.Name}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <label>Dupa data de inceput:</label>
+              <DatePicker
+                className="select"
+                onChange={(date, dateString) => {
+                  this.eventRequest.startDate = dateString;
+                }}
+                placeholder={'Alege Data'}
+                size="default"
+              />
+            </Modal>
           </div>
         </div>
       </div>
     );
   }
 
-  private async onSearchChange(data:any){
-    const { value } = data.target
-    console.log(value);
-    this.eventRequest.searchText=value
-    await this.getEvents()
+  private toggleShowModal() {
+    this.setState({
+      showFilter: !this.state.showFilter,
+    });
+  }
+  private async onSearchChange(data: any) {
+    this.eventDetilsContainer = new Array<EventDetailsViewModel>();
+    const { value } = data.target;
+    this.eventRequest.searchText = value;
+    this.getEvents(true);
   }
 
-  private async getEvents() {
+  private applyFilter() {
+    this.eventDetilsContainer = new Array<EventDetailsViewModel>();
+    this.getEvents(true);
+    this.toggleShowModal();
+  }
+
+  private async getEvents(search: boolean = false) {
     LoadingHelper.showLoading();
-    this.eventRequest.pageIndex = this.state.pageIndex;
+    this.eventRequest.pageIndex = search ? 0 : this.state.pageIndex;
     const result = await EventService.Find(this.eventRequest);
 
     this.eventDetilsContainer.push(...result.Data);
