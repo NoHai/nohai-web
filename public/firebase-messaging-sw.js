fd973c1a1ad883d6c74236845e064d1c81f794bc
Danut Ilie <daniel_ilie97@yahoo.com>
2019-11-19 13:09:47 +0200

    try fix notification

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,10 +15,13 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.setBackgroundMessageHandler(function(payload) {
+  var result = JSON.parse(payload);
   var notificationTitle = payload.notification.title; //or payload.notification or whatever your payload is
   var notificationOptions = {
     body: payload.notification.body,
     icon: payload.notification.icon,
+    time_to_live: result.time_to_live,
+    tag: result.tag,
     data: { url: payload.notification.click_action },
   };
 
