0b9e4695074f1b0f290e312456405e51ebfc6890
Danut Ilie <daniel_ilie97@yahoo.com>
2019-11-19 13:55:32 +0200

    test firebase-messaging-sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -28,7 +28,30 @@ messaging.setBackgroundMessageHandler(function(payload) {
   return self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
+self.addEventListener('push', function(event) {
+  console.log('Received a push message', event);
+
+  var title = 'Yay a message.';
+  var body = 'We have received a push message.';
+  var icon = 'YOUR_ICON';
+  var tag = 'simple-push-demo-notification-tag';
+  var data = {
+    doge: {
+        wow: 'such amaze notification data'
+    }
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      tag: tag,
+      data: data
+    })
+  );
+});
+
 self.addEventListener('notificationclick', function(event) {
-  //event.notification.close();
+  event.notification.close();
   event.waitUntil(self.clients.openWindow(event.notification.data));
 });
